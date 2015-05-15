2a3f20e2eca65226b9e20e93e042cc4a5ac4c811
Dave Rigby <daver@couchbase.com>
2015-05-15 12:21:45 +0100

    Convert getAllVBucketSequenceNumbers to C++03
    Change-Id: Iab41fcb010d168874339d37cf9c985b402191faf
    
    Conflicts:
    	src/ep_engine.cc

--- src/ep_engine.cc
+++ src/ep_engine.cc
@@ -5722,14 +5722,16 @@ ENGINE_ERROR_CODE EventuallyPersistentEngine::getAllVBucketSequenceNumbers(const
                             cookie);
     }
 
-    for (auto id : vbuckets) {
-        RCPtr<VBucket> vb = getVBucket(id);
+    for (std::vector<int>::iterator id = vbuckets.begin();
+         id != vbuckets.end();
+         ++id) {
+        RCPtr<VBucket> vb = getVBucket(*id);
         if (vb) {
-            auto state = vb->getState();
+            vbucket_state_t state = vb->getState();
             if (state == vbucket_state_active || state == vbucket_state_replica) {
-                uint16_t vbid = htons(static_cast<uint16_t>(id));
+                uint16_t vbid = htons(static_cast<uint16_t>(*id));
                 uint64_t highSeqno = htonll(vb->getHighSeqno());
-                auto offset = payload.size();
+                size_t offset = payload.size();
                 payload.resize(offset + sizeof(vbid) + sizeof(highSeqno));
                 memcpy(payload.data() + offset, &vbid, sizeof(vbid));
                 memcpy(payload.data() + offset + sizeof(vbid), &highSeqno,
